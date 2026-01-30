cd0f346dec6b777e926be8f9612fcd4902a942c8
Yaman Dhruw <84029287+yamandhruw750@users.noreply.github.com>
2026-01-30 04:59:57 +0000

    first commit

---
+++ TodoApp/src/context/TodoContext.js
@@ -0,0 +1,9 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext;
+
+export const TodoProvider = TodoContext.Provider;
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
